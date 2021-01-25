dd26d93a881c54ad652477b00bb0f474a8bcb95f
deepend-dev <deepayan.pandharkar@gmail.com>
2021-01-25 21:44:48 +0530

    update to login page

--- doctor-frontend/src/config/firebaseConfig.js
+++ doctor-frontend/src/config/firebaseConfig.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase';
+import * as firebase from 'firebase';
 
 const fireBaseConfig = {
   apiKey: "AIzaSyBCZfHHgEd0iYkGpXwFnPJLHrYHDOKmovQ",
@@ -6,7 +6,7 @@ const fireBaseConfig = {
   databaseURL: "https://doctor-doctor-e69b6.firebaseio.com",
   projectId: "doctor-doctor-e69b6",
   storageBucket: "doctor-doctor-e69b6.appspot.com",
-  messagingSenderId: "345689833495",
+  // messagingSenderId: "345689833495",
   appId: "1:345689833495:web:afdc743b5d1a5af8a51c52"
 };
 
@@ -14,6 +14,6 @@ const firebaseApp = firebase.initializeApp(fireBaseConfig);
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
-const providers = firebase.auth
+const googleAuthProvider = new firebase.auth.googleAuthProvider()
 
-export {db, auth, providers};
+export {db, auth, googleAuthProvider};
